f50fe8778652cb5a969cf86800295b62201763c9
dspray95 <dspray95@gmail.com>
2022-04-18 11:52:54 +0100

    fix: fixed vertice offset

--- src/js/engine/rendering/shader/Shader.js
+++ src/js/engine/rendering/shader/Shader.js
@@ -6,21 +6,6 @@ class Shader{
     static draw(){
         return
     }
-    
-    static drawVertLabels(canvas, vertices, mapWidth, terrainName, terrainColor){
-        canvas.fillStyle = "white"
-        canvas.font = "10px Arial"
-        for(let i = 0; i < mapWidth; i++){
-            canvas.fillText(i, vertices[i].screenSpaceX, vertices[i].screenSpaceY)
-        }
-        for(let i = vertices.length - mapWidth; i < vertices.length; i++){
-            canvas.fillText(i, vertices[i].screenSpaceX, vertices[i].screenSpaceY)
-        }
-        canvas.fillStyle = terrainColor
-        canvas.font = "40px Arial"
-        let centerVert = vertices[Math.floor(vertices.length/2)]
-        canvas.fillText(terrainName, centerVert.screenSpaceX, centerVert.screenSpaceY)
-    }
 
     static drawVetices(canvas, vertices, color=Color.YELLOW, size=5){
         canvas.fillStyle = color.toHtmlRgba()
@@ -34,6 +19,9 @@ class Shader{
         });
     }
 
+    static drawWireframe(canvas, triangles, vertices, color=Color.LIGHTGREY, strokeSize=1){
+        //TODO
+    }
 }
 
 export { Shader }
