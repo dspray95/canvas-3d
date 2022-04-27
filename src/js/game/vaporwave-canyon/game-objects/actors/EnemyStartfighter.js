99554e8c9bec8de66d1b8915244626653d8ea64b
dspray95 <dspray95@gmail.com>
2022-04-27 11:49:24 +0100

    chore: abstracted starfighter gameobject

--- src/js/game/vaporwave-canyon/game-objects/actors/EnemyStartfighter.js
+++ src/js/game/vaporwave-canyon/game-objects/actors/EnemyStartfighter.js
@@ -1,13 +1,12 @@
 import { Color } from "../../../../tools/Colors";
 import { Starfighter } from "./Starfighter";
 
-class Player extends Starfighter {
+class EnemyStarfighter extends Starfighter {
 
     constructor(location, parent){
-        super(location, parent, Color.ELECTRIC_BLUE, "player")
-        this.health = 3
+        super(location, parent, Color.RED< "enemy-starfighter")
     }
 
 }
 
-export { Player }
+export { EnemyStarfighter }
