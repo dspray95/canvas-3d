d9bf204504cd348d6fba9cb392e901293e53ae0d
dspray95 <dspray95@gmail.com>
2022-06-16 10:34:23 +0100

    chore: moved environment var to .env

--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,17 @@ import { DefaultConfig } from "./env/defaultConfig"
 import { DevConfig } from "./env/devConfig"
 import { ProdConfig } from "./env/prodConfig"
 
-const ENVIRONMENT = "dev"
+const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT
 let CONFIG = DefaultConfig
 
 if(ENVIRONMENT === "dev"){
     CONFIG = DevConfig
 }
-
-if(ENVIRONMENT === "prod"){
+else if(ENVIRONMENT === "prod"){
     CONFIG = ProdConfig
+} 
+else {
+    throw `Invalid environment set in .env. Expected 'dev' or 'prod', recieved ${ENVIRONMENT}`
 }
 
 
